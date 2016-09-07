db14250e4f642b51ca19345190a773d15ff15f8d
Hugo Lourenco <hugo.lourenco@outlook.com>
2016-09-07 23:28:41 +0100

    Resultado da pesquisa no google injectado na instancia praia e apresentado no layout da MainActivity

--- app/src/main/java/mei/meiweatherapp/MainActivity.java
+++ app/src/main/java/mei/meiweatherapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
@@ -12,11 +14,16 @@ import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.model.LatLng;
 
+import org.w3c.dom.Text;
+
 import mei.meiweatherapp.contratos.Praia;
 
 public class MainActivity extends FragmentActivity {
 
     String TAG = "<MyAutoComplete Google>";
+    TextView txtLat;
+    TextView txtLong;
+    TextView txtAdress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +32,9 @@ public class MainActivity extends FragmentActivity {
 
         PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
 
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener()
+        {
+
             @Override
             public void onPlaceSelected(Place place) {
                 Log.i(TAG, "Place: " + place.getName());
@@ -34,13 +43,22 @@ public class MainActivity extends FragmentActivity {
                 praia.setNome(place.getName().toString());
                 LatLng ll = place.getLatLng();
                 praia.setLatitude(Double.toString(ll.latitude));
-                praia.setLatitude(Double.toString(ll.longitude));
+                praia.setLongitude(Double.toString(ll.longitude));
+                praia.setMorada(place.getAddress().toString());
+
+                txtLat = (TextView) findViewById(R.id.txtLat);
+                txtLong = (TextView) findViewById(R.id.txtLong);
+                txtAdress = (TextView) findViewById(R.id.txtAdress);
+
+                txtLat.setText(praia.getLatitude());
+                txtLong.setText(praia.getLongitude());
+                txtAdress.setText(praia.getMorada());
             }
 
             @Override
             public void onError(Status status) {
-                // TODO: Handle the error.
                 Log.i(TAG, "An error occurred: " + status);
+                Toast.makeText(MainActivity.this, "Erro ao obter localização", Toast.LENGTH_LONG).show();
             }
         });
 
---
+++ app/src/main/java/mei/meiweatherapp/webservice/AccuWeatherWebService.java
@@ -0,0 +1,11 @@
+package mei.meiweatherapp.webservice;
+
+/**
+ * Created by Hugo on 06-Sep-16.
+ */
+public class AccuWeatherWebService extends WebserviceConnector {
+    private final String ENDPOINT = "";
+    private final String METHOD = "";
+
+
+}
