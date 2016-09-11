c33ded30b5cdc7ce47da99adb94dd11c7fc4aa62
Hugo Lourenco <hugo.lourenco@outlook.com>
2016-09-11 22:41:56 +0100

    CurrentCnditions 1st Step

--- app/src/main/java/mei/meiweatherapp/contratos/APIData.java
+++ app/src/main/java/mei/meiweatherapp/contratos/APIData.java
@@ -7,8 +7,8 @@ public class APIData {
         public static final String APIKEY = "vplLxDyBIiRtS44dUIX0NiGLNCZFpzW9";
         public static final String ENDPOINT = "http://dataservice.accuweather.com";
 
-        private static String METHOD_GeoPosition  = "/locations/v1/cities/geoposition/search?apikey={APIKEY}&q={LATITUDE}%2C{LONGITUDE}&language=pt-ptt&toplevel={MAISPROXIMA}";
-        private static String METHOD_CurrentConditions = "/currentconditions/v1/{LOCATIONKEY}?apikey={APIKEY}&details={DETAILS}";
+        private static String METHOD_GeoPosition  = ENDPOINT + "/locations/v1/cities/geoposition/search?apikey={APIKEY}&q={LATITUDE}%2C{LONGITUDE}&language=pt-pt&toplevel={MAISPROXIMA}";
+        private static String METHOD_CurrentConditions = ENDPOINT + "/currentconditions/v1/{LOCATIONKEY}?apikey={APIKEY}&details={DETAILS}&language=pt-pt";
 
         public static String getGeoPositionURL(String latitude, String longitude, Boolean localidadeMaisProxima)
         {
@@ -17,7 +17,7 @@ public class APIData {
 
         public static String getCurrentConditionURL(String locationKey, Boolean details)
         {
-            return METHOD_GeoPosition.replace("{APIKEY}", APIKEY).replace("{LOCATIONKEY}",locationKey).replace("{DETAILS}",details.toString());
+            return METHOD_CurrentConditions.replace("{APIKEY}", APIKEY).replace("{LOCATIONKEY}",locationKey).replace("{DETAILS}",details.toString());
         }
     }
 }
