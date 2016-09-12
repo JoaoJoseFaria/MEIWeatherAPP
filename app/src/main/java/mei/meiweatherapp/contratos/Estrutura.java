14f2da1ed4254dd55d83da1451768880d58dbd9e
Hugo Lourenco <hugo.lourenco@outlook.com>
2016-09-12 19:16:50 +0100

    comentario

--- app/src/main/java/mei/meiweatherapp/contratos/Estrutura.java
+++ app/src/main/java/mei/meiweatherapp/contratos/Estrutura.java
@@ -1,6 +1,6 @@
 package mei.meiweatherapp.contratos;
 
-//Estrutura
+//Estruturas
 public class Estrutura {
     public static final String NAME = "praias.db";
     public static final int VERSAO = 1;
